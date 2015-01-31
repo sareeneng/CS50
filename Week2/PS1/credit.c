881583f47478c66101dd44d1e2b8661838bb159e
Sareen Shah <sareen_eng@yahoo.com>
2015-01-31 15:02:22 -0600

    Organized Week 2 files into lecture files and problem set files

---
+++ Week2/PS1/credit.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+
+int main (void)
+{
+    long long num;
+    
+    printf("Number: ");
+    num = GetLongLong();
+    
+    
+}
---
+++ Week2/PS1/mario.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+
+int main(void)
+{
+    int h;
+    
+    do
+    {
+        printf("height: ");
+        h = GetInt();
+    }while(h<2);
+
+    int widthOfPyramid = h+1;
+
+    for(int i=0; i<h; i++)
+    {
+        //for each line, print a right-aligned hash
+        //first line should have two hashes
+        //so number of hashes = i+2
+        //e.g. in first line (i=0), two hashes
+        //in last line(i=h-1), h-1+2 hashes = h+1 hashes
+        
+        char currString[i+3];
+        currString[i+2]='\0'; // need to terminate each string
+        
+        for(int j=0; j<i+2; j++)
+           currString[j]='#';
+            
+        printf("%*s\n", widthOfPyramid,currString);
+        
+    }
+
+}
