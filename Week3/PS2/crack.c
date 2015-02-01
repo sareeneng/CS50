1c244c31662fcfa89a8be03bdc113fa10cfa284f
Sareen Shah <sareen_eng@yahoo.com>
2015-02-01 11:08:49 -0600

    in crack.c - cleaned up main function by making some subfunctions. Strings and chars and char*'s are extremely annoying

--- Week3/PS2/crack.c
+++ Week3/PS2/crack.c
@@ -5,7 +5,9 @@
 #include <cs50.h>
 #include <string.h>
 
-void dictionaryAttack(string, string);
+bool dictionaryAttack(string, string);
+char *GetSalt(string);
+char *GetKeyWithoutSalt(string);
 
 int main(int argc, string argv[])
 {
@@ -17,28 +19,22 @@ int main(int argc, string argv[])
     
     string fullKey = argv[1];
     
-    //Get salt, which is the first two letters of the encrypted key passed into the program
-    char salt[3];
-    salt[0]=fullKey[0];
-    salt[1]=fullKey[1];
-    salt[2]='\0';   //terminate string
+    string salt = GetSalt(fullKey);//Get salt, which is the first two letters of the encrypted key passed into the program
+    
+    if(dictionaryAttack(salt,fullKey)) //First let's run a dictionary attack and see if we get lucky
+        return 0;                       //If successful, end program. Otherwise, move on.
+    
     
     //Extract the key without the salt in it into a new string
-    char keyWithoutSalt[strlen(fullKey)-2];                     
-    strncpy(keyWithoutSalt, &fullKey[2],strlen(fullKey)-1);     //copies everything but 1st two chars
-    keyWithoutSalt[strlen(keyWithoutSalt)]='\0';                //Terminate string
-    
-   
+    char *keyWithoutSalt = malloc(strlen(fullKey-2));
+    keyWithoutSalt = GetKeyWithoutSalt(fullKey);                     
    
     //So now I have the salt and the key without the salt in two separate variables
-    //Next, let's try a dictionary attack first
-    
-    dictionaryAttack(salt,fullKey);
-        
+     
     return 0;
 }
 
-void dictionaryAttack(string salt, const string fullKey)
+bool dictionaryAttack(string salt, const string fullKey)
 {
     //Runs a dictionary attack on the salt to see if we can come up with the same key
     //Dictionary located at /usr/share/dict/words
@@ -58,7 +54,29 @@ void dictionaryAttack(string salt, const string fullKey)
         {
             printf("%s\n",unencDict);
             fclose(ifp);
+            return true;
         }
     }
     
+    return false;
+    
+}
+
+char *GetSalt(string fullKey)
+{
+    char *salt = malloc(3);
+    salt[0]=fullKey[0];
+    salt[1]=fullKey[1];
+    salt[2]='\0';   //terminate string
+    
+    return salt;   
+}
+
+char *GetKeyWithoutSalt(string fullKey)
+{
+    char *keyWithoutSalt = malloc(strlen(fullKey)-1);
+    strncpy(keyWithoutSalt, &fullKey[2],strlen(fullKey)-1);     //copies everything but 1st two chars
+    keyWithoutSalt[strlen(keyWithoutSalt)]='\0';                //Terminate string
+
+    return keyWithoutSalt;
 }
