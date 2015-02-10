b36404a8c359cabaed276ce23cb3806394c122c5
Sareen Shah <sareen_eng@yahoo.com>
2015-02-10 13:27:38 -0600

    finished function load in dictionary.c, so that it loads into a trie. May be able to make it cleaner, will come back to that later.

--- Week7/PS5/dictionary.c
+++ Week7/PS5/dictionary.c
@@ -8,8 +8,21 @@
  ***************************************************************************/
 
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #include "dictionary.h"
+
+#define MAXWORDLENGTH 45 
+
+struct trieNode
+{
+    bool isWord;
+    struct trieNode* children[27];  //An array of memory addresses to other trieNodes
+};   
+
+void add(char*, struct trieNode*);
+
 
 /**
  * Returns true if word is in dictionary else false.
@@ -25,7 +38,31 @@ bool check(const char* word)
  */
 bool load(const char* dictionary)
 {
-    // TODO
+    //We'll load using a trie
+    // The root node will have 27 characters (including '). 
+    // Each child will point to a new node as necessary
+    // root node should have word=false
+    
+    
+    //Establish root
+    struct trieNode* root = malloc(sizeof(struct trieNode));
+    root->isWord=false;
+    
+    //Not sure if this is necessary yet
+    for(int i=0; i<27; i++)
+        root->children[i]=NULL;
+        
+    char currWord[MAXWORDLENGTH+2]; //add room for '\n' and '\0'
+    
+    FILE* file;
+    file = fopen(dictionary,"r");
+    
+    if(file == NULL)
+        return false;
+    
+    while(fgets(currWord, MAXWORDLENGTH+2, file)  )
+        add(currWord, root);        //Pass in the memory address to the trie Node root       
+        
     return false;
 }
 
@@ -45,4 +82,30 @@ bool unload(void)
 {
     // TODO
     return false;
+}
+
+void add(char* currWord, struct trieNode* parent)
+{  
+    if(currWord[0]=='\n')
+        parent->isWord=true;
+        
+    else
+    {
+        int index;
+        if(currWord[0]=='\'')
+            index = 26;
+        else
+            index = currWord[0]-97;
+        
+        if(parent->children[index]==NULL)
+        {
+            struct trieNode* newNode = malloc(sizeof(struct trieNode));
+            newNode->isWord = false;
+            for(int i=0; i<27; i++)
+                newNode->children[i]=NULL;
+            parent->children[index] = newNode;
+
+        }
+        add(&currWord[1], parent->children[index]);
+    }
 }
